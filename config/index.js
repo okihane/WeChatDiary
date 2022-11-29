8e38fdb359b03e8ea8aef2979b71fc01cf265120
PengLei <68989992+vip-lp@users.noreply.github.com>
2022-11-29 15:16:35 +0800

    第六次更新，微信公众号的模板ID

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const CONFIG = {
   APP_SECRET: "a861a605f1e81cc2ecc6ac658a984a4b",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "BkiHlZFGftJbb1vEgnljcCFlPPlK0PDBXLFQ7f2cc4M",
+  TEMPLATE_ID: "nQChXCbxXhDXSy2VD7hrzMmFSZWxw8UpCmTHKDJCj0k",
 
   // 天行API的key
   TXApiKey: "a6740bc32d75260e46a7d56644c79f34",
